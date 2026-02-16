fea5ca6fbccf7a77ad4f3ab53052f7729f455773
dahbner <dabnerorozco123@gmail.com>
2026-02-16 18:55:53 -0400

    feat: soporte para ingles y espanol

--- src/presenter.js
+++ src/presenter.js
@@ -3,6 +3,7 @@ import saludar from "./saludador";
 const nombreInput = document.querySelector("#nombre");
 const edadInput = document.querySelector("#edad");
 const generoInput = document.querySelector("#genero");
+const idiomaInput = document.querySelector("#idioma");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
 
@@ -12,6 +13,7 @@ form.addEventListener("submit", (event) => {
   const nombre = nombreInput.value;  
   const edad = Number.parseInt(edadInput.value);
   const genero = generoInput.value;
+  const idioma = idiomaInput.value;
 
-  div.innerHTML = "<p>" + saludar(nombre, genero, edad) + "</p>";
+  div.innerHTML = "<p>" + saludar(nombre, genero, edad, idioma) + "</p>";
 });
--- src/saludador.js
+++ src/saludador.js
@@ -1,47 +1,55 @@
-function saludar(nombre, genero, edad) 
+function saludar(nombre, genero, edad, idioma) 
 {
   let fechaActual = new Date();
   let horaActual = fechaActual.getHours();
   let saludo = "";
 
-  if (horaActual < 12) 
-  {
-    saludo = "Buenos días";
-  } 
-  else if (horaActual < 19) 
-  {
-    saludo = "Buenas tardes";
-  } 
-  else 
-  {
-    saludo = "Buenas noches";
+if (idioma === "en") {
+    if (horaActual < 12) {
+      saludo = "Good morning ";
+    } else if (horaActual < 19) {
+      saludo = "Good afternoon ";
+    } else {
+      saludo = "Good evening ";
+    }
+  } else {
+    if (horaActual < 12) {
+      saludo = "Buenos dias ";
+    } else if (horaActual < 19) {
+      saludo = "Buenas tardes ";
+    } else {
+      saludo = "Buenas noches ";
+    }
   }
 
   let trato = "";
-  if (edad > 30) 
-    {
-      if (genero === "M") 
-        {
-      trato = "Sr."; 
-      } 
-      else 
-      {
-      trato = "Sra."; 
+  if (idioma === "en") {
+    if (edad > 30) {
+      if (genero === "M") {
+        trato = "Mr. ";
+      } else {
+        trato = "Mrs. ";
       }
-    } 
-    else 
-    {
-      if (genero === "M") 
-      {
-        trato = "estimado "; 
-      } 
-      else 
-      {
-        trato = "estimada "; 
+    } else {
+      trato = "Dear ";
+    }
+  } else {
+    if (edad > 30) {
+      if (genero === "M") {
+        trato = "Sr.";
+      } else {
+        trato = "Sra. ";
+      }
+    } else {
+      if (genero === "M") {
+        trato = "estimado ";
+      } else {
+        trato = "estimada ";
       }
     }
+  }
 
-  return saludo + " " + trato + nombre;
+  return saludo + " " + trato + " " + nombre;
 }
 
 export default saludar;
