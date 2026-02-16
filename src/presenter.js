6ddf352fb0b2bc884abf699155ea52277c8b8281
dahbner <dabnerorozco123@gmail.com>
2026-02-16 18:40:29 -0400

    feat: saludar por genero usando estimado/a

--- src/presenter.js
+++ src/presenter.js
@@ -1,6 +1,7 @@
 import saludar from "./saludador";
 
 const nombreInput = document.querySelector("#nombre");
+const generoInput = document.querySelector("#genero");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
 
@@ -8,6 +9,7 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const nombre = nombreInput.value;  
+  const genero = generoInput.value;
 
-  div.innerHTML = "<p>" + saludar(nombre) + "</p>";
+  div.innerHTML = "<p>" + saludar(nombre, genero) + "</p>";
 });
